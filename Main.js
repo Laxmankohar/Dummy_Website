2fdc3a1d83aada2c2471ab37abf01a76b43a61b9
Laxman Kohar <lkverma5422@gmail.com>
2022-03-17 11:57:28 +0530

    project completed

---
+++ Main.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    var x = document.getElementById("mm");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
